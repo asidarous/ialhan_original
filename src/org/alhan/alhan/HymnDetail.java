05da8e506aecef3073eac0854800d1a8cc4590c1
asidarous <asidarous@gmail.com>
2014-04-09 23:09:19 -0700

    added English text and language toggle capabilities

--- src/org/alhan/alhan/HymnDetail.java
+++ src/org/alhan/alhan/HymnDetail.java
@@ -5,32 +5,59 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class HymnDetail extends Activity {
-
+	ToggleButton tb;
+	TextView tx1;
+	String copticText, englishText;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.coptic_hymn_text);
-		String copticText = getCopticTextFromQuery();
-		TextView tx1 = (TextView) findViewById(R.id.copticHymnView);
-		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/CS_Avva_Shenouda.ttf");
-		tx1.setTypeface(font);
-		tx1.setText(copticText);
+		setContentView(R.layout.hymn_detail_view);
+		getCopticTextFromQuery();
+		tx1 = (TextView) findViewById(R.id.hymnView);
+		
+		setCopticText();
+
+		tb = (ToggleButton) findViewById(R.id.languageButton);
+		tb.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				 if (tb.getText().toString().equalsIgnoreCase("English")) {
+					 setCopticText();
+				 } else {
+					 Typeface font = Typeface.DEFAULT;
+					 tx1.setTypeface(font);
+					 tx1.setText(englishText);
+				 }
+			}
+		});
 
 	}
 
-	private String getCopticTextFromQuery() {
+	private void setCopticText() {
+		Typeface font = Typeface.createFromAsset(getAssets(),"fonts/CS_Avva_Shenouda.ttf");
+		 tx1.setTypeface(font);
+		 tx1.setText(copticText);
+	}
+
+	private void getCopticTextFromQuery() {
 		DBOpenHelper dbOpenHelper = new DBOpenHelper(this);
 		SQLiteDatabase db = dbOpenHelper.getDB();
+
+		String hymnId = getIntent().getStringExtra("hymn_id");
+		Cursor cursor = db.query("hymn", new String[] { "hymn_coptic, hymn_english",
+				"hymn_english" }, "hymn_id = " + Integer.parseInt(hymnId),
+				null, null, null, null);
+		cursor.moveToFirst();
+		copticText = cursor.getString(cursor.getColumnIndex("hymn_coptic"));
+		englishText = cursor.getString(cursor.getColumnIndex("hymn_english"));
 		
-        String hymnId = getIntent().getStringExtra("hymn_id");
-        Cursor cursor = db.query("hymn", new String[]{"hymn_coptic","hymn_english"}, "hymn_id = "+Integer.parseInt(hymnId), null, null,null, null);
-        cursor.moveToFirst();
-        String copticText = cursor.getString(cursor.getColumnIndex("hymn_coptic"));
-        return copticText;
 	}
 
 }
